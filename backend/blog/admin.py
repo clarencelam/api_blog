18a9da6a37eb201267fcfb0b2ffe70594d2efa3a
clam <lam.clarence.bc@gmail.com>
2022-02-07 23:12:23 -0800

    progress

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BlogPost
 
-# Register your models here.
+# Apply summernote to all TextField in model.
+class BlogPostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = ('content',) #Text fields named "content" will be a summernote widget
+
+admin.site.register(BlogPost, BlogPostAdmin)
