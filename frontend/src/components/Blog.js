13dde123bfca3efea81193748bd668ac3f6c15db
clam <lam.clarence.bc@gmail.com>
2022-03-20 17:59:15 -0700

    updats to frontend

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,6 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Blog = () => {
-}
+    const [blogs, setBlogs] = useState([]);
+    const [featuredBlog, setFeaturedBlog] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/featured`);
+                setFeaturedBlog(res.data[0]); 
+            }
+            catch (err) {
+
+            }
+        }
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/`);
+                setBlogs(res.data);
+            }
+            catch (err) {
+
+            }
+        }
+        fetchBlogs();
+    }, []);
+
+    const capitalizeFirstLetter = (word) => {
+        if (word)
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        return '';
+    };
+
+    const getBlogs = () => {
+
+    }
+
+    return (
+        <div className='container mt-3'>
+            <div class="nav-scroller py-1 mb-2">
+                <nav class="nav d-flex justify-content-between">
+                    <Link className="p-2 text-muted" to='/category/investing'>Investing</Link>
+                    <Link className="p-2 text-muted" to='/category/lifestyle'>Lifestyle</Link>
+                    <Link className="p-2 text-muted" to='/category/misc'>Misc</Link>
+                </nav>
+            </div>
+
+            <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+                <div className="col-md-6 px-0">
+                    <h1 className="display-4 font-italic">{featuredBlog.title}</h1>
+                    <p className="lead my-3">{featuredBlog.excerpt}</p>
+                    <p className="lead mb-0">
+                        <Link to={`/blog/${featuredBlog.slug}`} class="text-white fw-bold">
+                            Continue reading...
+                        </Link></p>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    )
+};
 
 export default Blog;
