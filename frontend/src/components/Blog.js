2176a1824dddebf3056716ee0b903fd7caec1e40
clam <lam.clarence.bc@gmail.com>
2022-03-21 19:32:45 -0700

    add featured posts on blog frontend

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -10,7 +10,7 @@ const Blog = () => {
         const fetchData = async () => {
             try {
                 const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/featured`);
-                setFeaturedBlog(res.data[0]); 
+                setFeaturedBlog(res.data[0]);
             }
             catch (err) {
 
@@ -39,13 +39,45 @@ const Blog = () => {
     };
 
     const getBlogs = () => {
+        let list = [];
+        let result = [];
 
+        blogs.map(blogPost => {
+            return list.push(
+                <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div className="col p-4 d-flex flex-column position-static">
+                        <strong className="d-inline-block mb-2 text-primary">{blogPost.category}</strong>
+                        <h3 className="mb-0">Featured post</h3>
+                        <div className="mb-1 text-muted">Nov 12</div>
+                        <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" className="stretched-link">Continue reading</a>
+                    </div>
+                    <div className="col-auto d-none d-lg-block">
+                        <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    </div>
+                </div>
+            );
+        });
+
+        for (let i = 0; i < list.length; i += 2) {
+            result.push(
+                <div key={i} className="row mb-2">
+                    <div className="col-md-6">
+                        {list[i]}
+                    </div>
+                    <div className="col-md-6">
+                        {list[i + 1] ? list[i + 1] : null}
+                    </div>
+                </div>
+            )
+        }
+        return result;
     }
 
     return (
         <div className='container mt-3'>
-            <div class="nav-scroller py-1 mb-2">
-                <nav class="nav d-flex justify-content-between">
+            <div className="nav-scroller py-1 mb-2">
+                <nav className="nav d-flex justify-content-between">
                     <Link className="p-2 text-muted" to='/category/investing'>Investing</Link>
                     <Link className="p-2 text-muted" to='/category/lifestyle'>Lifestyle</Link>
                     <Link className="p-2 text-muted" to='/category/misc'>Misc</Link>
@@ -62,6 +94,8 @@ const Blog = () => {
                         </Link></p>
                 </div>
             </div>
+
+            {getBlogs()}
 
 
 
