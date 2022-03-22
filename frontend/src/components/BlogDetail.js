092132ba7415930f7ae8c649339da29a76aab82d
clam <lam.clarence.bc@gmail.com>
2022-03-22 00:28:19 -0700

    blog detail page progress

--- frontend/src/components/BlogDetail.js
+++ frontend/src/components/BlogDetail.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const blogdetail = () => (
-    <div>
-        blogdetail
-    </div>
-);
+const BlogDetail = () => {
+    const [blog, setBlog] = useState({});
 
-export default blogdetail;
+
+
+    return(
+        <div className='container mt-3'>
+            <h1 className='display-2'>title</h1>
+            <h2 className="text-muted mt-3">Category: category</h2>
+            <h4>month day</h4>
+            <div className='mt-5 mb-5' dangerouslySetInnerHTML={createBlog()} />
+            <hr />
+            <p className='lead mb-5'><Link to='/blog' className='font-weight-bold'>Back to Blogs</Link></p>
+        </div>
+    );
+};
+
+export default BlogDetail;
